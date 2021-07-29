48273d3c8338e1dd18cfca2b6d9c478f5150dd0c
yuj120254 <yuj120254@gmail.com>
2021-07-28 21:11:33 -0400

    Renamed and updated files

---
+++ Generate_graphene_parameters_main.m
@@ -0,0 +1,208 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This file contains the code and examples to access the 5 different forms
+% of the Graphene_paramters_function. 
+% To run this file, you must have Graphene_lattice.m and
+% Graphene_paramters_function.m in the same folder.
+%%%%%%%%%
+% The Graphene_paramters_function ran in this file can handle 5 different
+% sets of inputs, each dealing with a different case of He on graphene, the
+% cases are differentiated by the number of inputs, ranging from 3 to 7.
+%
+% The first 2 inputs does not change with the different cases and are:
+% (strain type, array of strain indicies, .....)
+% The strain type is either "isotropic" or "anisotropic"
+% The array of strain indicies is an array of the indicies of the strain
+% that you wish to generate graphene parameters for. To figure out which
+% index corresponds to which strain, look at the params array below.
+%
+% The remaining inputs determine the case to calculate the parameters for
+% Case 1: (mainly for testing)
+% There are only 3 inputs: (strain type, strain indicies, "min")
+% In this case, for each strain, the minimum of V(0,0,z) is determined, and
+% the rest of the graphene parameters are calculated at that z.
+%
+% Case 2: (mainly for testing)
+% There are 4 inputs: (strain type, strain indicies, "z", height(Angstroms))
+% In this case, for each strain, the graphene parameters are calculated at
+% the z specified.
+%
+% Case 3:
+% There are 5 inputs: (strain type, strain indicies, "averaging", z, rho)
+% In this case, the graphene parameters are calculated such that the
+% potential is a weighted average in z using the weight rho. This case
+% currently only supports calculations for a single value of strain.
+%
+% Case 4:
+% There are 6 inputs: (strain type, strain indicies, "external_isotropic", V_min, V_sp, V_max)
+% In this case, the graphene fourier components are calculated from the
+% values of  V_min, V_sp, and V_max taken from some external potential, and
+% the rest of the parameters are calculated from them. This case
+% currently only supports calculations for a single value of strain.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% THIS DOES NOT CURRENTLY WORK
+% Case 5: 
+% There are 7 inputs: (strain type, strain indicies, "external_isotropic", V_min, V_sp_para, V_sp_perp, V_max)
+% In this case, the graphene fourier components are calculated from the
+% values of  V_min, V_sp, and V_max taken from some external potential, and
+% the rest of the parameters are calculated from them. This case
+% currently only supports calculations for a single value of strain.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% THIS DOES NOT CURRENTLY WORK
+
+% the paramters sigma and epsilon for all strain, using the latest values.
+% The layout is:
+% [[ strains .... ],
+%  [ sigmas .... ],
+%  [ epsilons .... ]]
+anisotropic_params = [[0.00,0.05,0.10,0.15,0.16,0.17,0.18,0.19,0.20,0.21,0.22,0.23,0.24,0.25,0.26,0.27,0.28,0.29,0.34],
+                      [2.643,2.669,2.700,2.738,2.749,2.759,2.769,2.780,2.791,2.804,2.817,2.831,2.846,2.858,2.878,2.896,2.917,2.938,3.065],
+                      [16.961,16.758,16.530,16.256,16.170,16.086,16.001,15.911,15.825,15.720,15.618,15.510,15.400,15.303,15.156,15.025,14.882,14.733,13.963]];
+
+isotropic_params = [[0.00,0.05,0.10,0.15,0.20,0.25,0.30],
+                    [2.642,2.697,2.749,2.800,2.849,2.897,2.942],
+                    [16.968,16.585,16.248,15.952,15.693,15.463,15.259]];
+% Do not comment this out ^
+                
+             
+    
+% From here on uncomment the case which you need
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Case 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% % set the type of strain
+% strain_type = "anisotropic";
+% 
+% % set the strains for which parameters are to be calclated
+% is_array = [1,2,3,4,9,14];%
+% 
+% % display the strains for which parameters are to be calclated
+% disp(strcat("computing graphene paramters for ", strain_type, " strain:"));
+% for s = is_array
+%     strain = anisotropic_params(1, s)
+% end
+% 
+% % Calls the functions
+% [V, V_lat, V_fc, lattice_vectors, grid, param, strain_string] = ...
+%     Graphene_parameters_function(strain_type, is_array, "min");
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% End Case 1 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Case 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% % set the type of strain
+% strain_type = "anisotropic";
+% 
+% % set the strains for which parameters are to be calclated
+% is_array = [1,2,3,4,9,14];%
+% 
+% % set the height at which parameters are to be calclated (must be the same
+% % lengths as is_array)
+% z_array = [2.5,2.5,2.5,2.5,2.5,2.5];%
+% 
+% % display the strains for which parameters are to be calclated
+% disp(strcat("computing graphene paramters for ", strain_type, " strain:"));
+% for s = is_array
+%     strain = anisotropic_params(1, s)
+% end
+% 
+% % Calls the functions
+% [V, V_lat, V_fc, lattice_vectors, grid, param, strain_string] = ...
+%     Graphene_parameters_function(strain_type, is_array, "z", z_array);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% End Case 2 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Case 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% % set the type of strain
+% strain_type = "isotropic";
+% 
+% % set the strains for which parameters are to be calclated
+% is_array = [1,2,3,4];%
+% 
+% % load density in z for V0
+% z_rho_V0 = dlmread("V0_rho0.txt");
+% z_V0 = z_rho_V0(200:5:350,1);
+% rho_V0 = z_rho_V0(200:5:350,2)/sum(z_rho_V0(200:5:350,2));% Important: always normalize rho here
+% 
+% z_array = [z_V0; z_V0; z_V0; z_V0];
+% rho_array = [rho_V0; rho_V0; rho_V0; rho_V0];
+% 
+% % display the strains for which parameters are to be calclated
+% disp(strcat("computing graphene paramters for ", strain_type, " strain:"));
+% for s = is_array
+%     strain = anisotropic_params(1, s)
+% end
+% 
+% % Calls the functions
+% [V, V_lat, V_fc, lattice_vectors, grid, param, strain_string] = ...
+%     Graphene_parameters_function(strain_type, is_array, "average", z_array, rho_array);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% End Case 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Case 4 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% % set the type of strain
+% strain_type = "isotropic";
+% 
+% % set the strains for which parameters are to be calclated
+% is_array = [1,2,3,4];%
+% 
+% % load density in z for V0
+% min_array = [0, 0, 0, 0];
+% sp_array = [65.98, 65.98, 65.98, 65.98];
+% max_array = [72.18, 72.18, 72.18, 72.18];
+% 
+% % display the strains for which parameters are to be calclated
+% disp(strcat("computing graphene paramters for ", strain_type, " strain:"));
+% for s = is_array
+%     strain = anisotropic_params(1, s)
+% end
+% 
+% % Calls the functions
+% [V, V_lat, V_fc, lattice_vectors, grid, param, strain_string] = ...
+%     Graphene_parameters_function(strain_type, is_array, "external_isotropic", min_array, sp_array, max_array);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% End Case 4 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Case 5 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% % set the type of strain
+% strain_type = "isotropic";
+% 
+% % set the strains for which parameters are to be calclated
+% is_array = [1,2,3,4];%
+% 
+% % load density in z for V0
+% min_array = [0, 0, 0, 0];
+% sp_para_array = [65.98, 65.98, 65.98, 65.98];
+% sp_perp_array = [65.98, 65.98, 65.98, 65.98];
+% max_array = [72.18, 72.18, 72.18, 72.18];
+% 
+% % display the strains for which parameters are to be calclated
+% disp(strcat("computing graphene paramters for ", strain_type, " strain:"));
+% for s = is_array
+%     strain = anisotropic_params(1, s)
+% end
+% 
+% % Calls the functions
+% [V, V_lat, V_fc, lattice_vectors, grid, param, strain_string] = ...
+%     Graphene_parameters_function(strain_type, is_array, "external_anisotropic", ...
+%     min_array, sp_para_array, sp_perp_array, max_array);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% End Case 5 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
---
+++ Graphene_parameters_function.m
@@ -0,0 +1,372 @@
+%%%%%%%%%%%%%%%%
+% function to generate all the parameters necessary for the
+% calculations
+% Takes a varying number of paramters, there are currently 2 numbers
+% accepted:
+% If you want to calculate graphene parameters for a minimum of
+% V(0,0,z), the inputs are (strain_type, is, "min")
+% If you want to calculate graphene parameters for a specific value of
+% z, the inputs are (strain_type, is, "z", 2.5), here 2.5 is the
+% specified value, only change that.
+%%%%%%%%%%%%%%%%
+
+function [V, V_lat, V_fc, lattice_vectors, grid, param, strain_string] = Graphene_paramters_function(varargin)
+    % sets the grid in z and C-C distance a0
+    z = [2:0.01:6]; % In angstroms This is only for minimizing V in z, not used in other options
+    a0_real = 1.42; % In angstroms
+    poisson = 0.165; % poisson ratio, for anisotropic strain
+    
+    % the paramters sigma and epsilon for all strain, using the latest values
+    anisotropic_params = [[0.00,0.05,0.10,0.15,0.16,0.17,0.18,0.19,0.20,0.21,0.22,0.23,0.24,0.25,0.26,0.27,0.28,0.29,0.34],
+                      [2.643,2.669,2.700,2.738,2.749,2.759,2.769,2.780,2.791,2.804,2.817,2.831,2.846,2.858,2.878,2.896,2.917,2.938,3.065],
+                      [16.961,16.758,16.530,16.256,16.170,16.086,16.001,15.911,15.825,15.720,15.618,15.510,15.400,15.303,15.156,15.025,14.882,14.733,13.963]];
+ 
+    isotropic_params = [[0.00,0.05,0.10,0.15,0.20,0.25,0.30],
+                      [2.642,2.697,2.749,2.800,2.849,2.897,2.942],
+                      [16.968,16.585,16.248,15.952,15.693,15.463,15.259]];
+   
+    % array of strain values that we are calculating parameters for
+    is_array = varargin{2};
+    iss = 1
+    
+    % loop over strain
+    for is = is_array
+        % establish parameters for isotropic of anisotropic strain
+        if varargin{1} == "isotropic" 
+            % C-C distance
+            a0_real = (1 + isotropic_params(1, is)) * 1.42; % In angstroms
+
+            % strain
+            delta = 0;
+            strain_string = sprintf("i%02d", 100*isotropic_params(1, is));
+
+            sigma = isotropic_params(2, is); % In angstroms
+            epsilon = isotropic_params(3, is); % In Kelvin
+        elseif varargin{1} == "anisotropic" 
+            % C-C distance
+            a0_real = 1.42; % In angstroms
+
+            % strain
+            delta = anisotropic_params(1, is);
+            strain_string = sprintf("a%02d", 100*anisotropic_params(1, is));
+
+            sigma = anisotropic_params(2, is); % In angstroms
+            epsilon = anisotropic_params(3, is); % In Kelvin
+        end
+
+        % lattice vectors
+        a1_real = [(sqrt(3)*a0_real/2)*(1 - delta*poisson), (3*a0_real/2)*(1 + delta)]; % In angstroms
+        a2_real = [-a1_real(1), a1_real(2)]; % In angstroms
+        a3_real = a1_real-a2_real; % In angstroms
+
+        % normalization for all lattice vectors such that their length is pi
+        space_normalization = pi/norm(a1_real);
+        z_nondim = z*space_normalization;
+        sigma_nondim = sigma*space_normalization;
+        a0_nondim = a0_real*space_normalization;
+        a1_nondim = a1_real*space_normalization;
+        a2_nondim = a2_real*space_normalization;
+        a3_nondim = a3_real*space_normalization;
+            
+        % creates the Graphene_Lattice class for all subsequent calculations
+        glat = Graphene_Lattice(sigma_nondim, delta, a0_nondim, a1_nondim, a2_nondim);
+
+        % scaling factors for calculations
+        prefactor = epsilon*glat.sigma*glat.sigma*2*pi/glat.A;
+
+        h = 1.05457e-34; % kg m^2 /s (hbar)
+        m = 6.6464767e-27; % kg
+
+        E_R = h^2 * pi^2 / (2 * m * norm(a1_real*10^-10)^2); % k # ????24ma^2????
+        k_B = 1.380649e-23; % J / K
+
+        % scale for energy
+        energy_scale = k_B/E_R;
+
+        % chooses if we calculate the potential at the minimum of V in z,
+        % or at the value of z input
+        if nargin == 3
+            % generates V-He-Graphene at (x,y)=(0,0) to find the minimum of z
+            Va = V_z_0(z_nondim, 2, glat);
+
+            [~, I] = min(Va);
+
+            zmin_nondim = z_nondim(I);
+
+        elseif nargin == 4
+            % manually sets the z at which all the rest of the calculation happens
+            zmin_nondim = varargin{4}(iss)*space_normalization;
+        else % just some random value so the next part of the code runs
+            zmin_nondim = 2;
+        end
+
+        % sets the number of fourier components taken into account for the
+        % calculations and calculates the fourier components
+        gterms = 2;
+        
+        if nargin == 6
+            if varargin{1} == "anisotropic" 
+                disp("wrong type of strain")
+                break
+            end
+            
+            Vf1_array = zeros(5,5);
+            Vf1_array(2,2) = 1;
+            Vf1_array(2,3) = 1;
+            Vf1_array(3,2) = 1;
+            Vf1_array(3,4) = 1;
+            Vf1_array(4,3) = 1;
+            Vf1_array(4,4) = 1;
+            
+            Vf2_array = zeros(5,5);
+            Vf2_array(1,2) = 1;
+            Vf2_array(2,1) = 1;
+            Vf2_array(4,2) = 1;
+            Vf2_array(2,4) = 1;
+            Vf2_array(4,5) = 1;
+            Vf2_array(5,4) = 1;
+
+            r_min = [0, 0];
+            r_max = (a1_nondim+a2_nondim)/3;
+            r_sp = a1_nondim/2;
+            
+            V1_min = real(V_fourier(r_min(1), r_min(2), gterms, Vf1_array, 1, glat))/energy_scale;
+            V2_min = real(V_fourier(r_min(1), r_min(2), gterms, Vf2_array, 1, glat))/energy_scale;
+            V1_max = real(V_fourier(r_max(1), r_max(2), gterms, Vf1_array, 1, glat))/energy_scale;
+            V2_max = real(V_fourier(r_max(1), r_max(2), gterms, Vf2_array, 1, glat))/energy_scale;
+            V1_sp = real(V_fourier(r_sp(1), r_sp(2), gterms, Vf1_array, 1, glat))/energy_scale;
+            V2_sp = real(V_fourier(r_sp(1), r_sp(2), gterms, Vf2_array, 1, glat))/energy_scale;
+            
+            a = V1_max - V1_min;
+            b = V2_max - V2_min;
+            c = V1_sp - V1_min;
+            d = V2_sp - V2_min;
+
+            A = [a b; c d]*energy_scale;
+            
+            external_potential_params = [varargin{6}(iss)-varargin{4}(iss), varargin{5}(iss)-varargin{4}(iss)]';
+            
+            Vf_fit = A\external_potential_params;
+            
+            Vfourier = (Vf_fit(1)*Vf1_array + Vf_fit(2)*Vf2_array);
+            Vfourier_scaled = Vfourier*energy_scale;
+        elseif nargin == 7  
+            if varargin{1} == "isotropic" 
+                disp("wrong type of strain")
+                break
+            end
+       
+            Vf1_array = zeros(5,5);
+            Vf1_array(2,2) = 1;
+            Vf1_array(4,4) = 1;
+            
+            Vf2_array = zeros(5,5);
+            Vf2_array(2,3) = 1;
+            Vf2_array(3,2) = 1;
+            Vf2_array(3,4) = 1;
+            Vf2_array(4,3) = 1;
+            
+            Vf3_array = zeros(5,5);
+            Vf3_array(1,2) = 1;
+            Vf3_array(2,1) = 1;
+            Vf3_array(4,5) = 1;
+            Vf3_array(5,4) = 1;
+
+            Vf4_array = zeros(5,5);
+            Vf4_array(4,2) = 1;
+            Vf4_array(2,4) = 1;
+            
+            r_min = [0, 0];
+            r_max_para = (a1_nondim+a2_nondim)/3;
+            r_sp_para = a1_nondim/2;
+            r_max_perp = a3_nondim/2 + r_max_para/2;
+            r_sp_perp = a3_nondim/2;
+
+            V1_min = real(V_fourier(r_min(1), r_min(2), gterms, Vf1_array, 1, glat))/energy_scale;
+            V2_min = real(V_fourier(r_min(1), r_min(2), gterms, Vf2_array, 1, glat))/energy_scale;
+            V3_min = real(V_fourier(r_min(1), r_min(2), gterms, Vf3_array, 1, glat))/energy_scale;
+            V4_min = real(V_fourier(r_min(1), r_min(2), gterms, Vf4_array, 1, glat))/energy_scale;
+            V1_max_para = real(V_fourier(r_max_para(1), r_max_para(2), gterms, Vf1_array, 1, glat))/energy_scale;
+            V2_max_para = real(V_fourier(r_max_para(1), r_max_para(2), gterms, Vf2_array, 1, glat))/energy_scale;
+            V3_max_para = real(V_fourier(r_max_para(1), r_max_para(2), gterms, Vf3_array, 1, glat))/energy_scale;
+            V4_max_para = real(V_fourier(r_max_para(1), r_max_para(2), gterms, Vf4_array, 1, glat))/energy_scale;
+            V1_sp_para = real(V_fourier(r_sp_para(1), r_sp_para(2), gterms, Vf1_array, 1, glat))/energy_scale;
+            V2_sp_para = real(V_fourier(r_sp_para(1), r_sp_para(2), gterms, Vf2_array, 1, glat))/energy_scale;
+            V3_sp_para = real(V_fourier(r_sp_para(1), r_sp_para(2), gterms, Vf3_array, 1, glat))/energy_scale;
+            V4_sp_para = real(V_fourier(r_sp_para(1), r_sp_para(2), gterms, Vf3_array, 1, glat))/energy_scale;
+            V1_max_perp = real(V_fourier(r_max_perp(1), r_max_perp(2), gterms, Vf1_array, 1, glat))/energy_scale;
+            V2_max_perp = real(V_fourier(r_max_perp(1), r_max_perp(2), gterms, Vf2_array, 1, glat))/energy_scale;
+            V3_max_perp = real(V_fourier(r_max_perp(1), r_max_perp(2), gterms, Vf3_array, 1, glat))/energy_scale;
+            V4_max_perp = real(V_fourier(r_max_perp(1), r_max_perp(2), gterms, Vf4_array, 1, glat))/energy_scale;
+            V1_sp_perp = real(V_fourier(r_sp_perp(1), r_sp_perp(2), gterms, Vf1_array, 1, glat))/energy_scale;
+            V2_sp_perp = real(V_fourier(r_sp_perp(1), r_sp_perp(2), gterms, Vf2_array, 1, glat))/energy_scale;
+            V3_sp_perp = real(V_fourier(r_sp_perp(1), r_sp_perp(2), gterms, Vf3_array, 1, glat))/energy_scale;
+            V4_sp_perp = real(V_fourier(r_sp_perp(1), r_sp_perp(2), gterms, Vf4_array, 1, glat))/energy_scale;
+
+            a = V1_max_para - V1_min;
+            b = V2_max_para - V2_min;
+            c = V3_max_para - V3_min;
+            d = V4_max_para - V4_min;
+            e = V1_sp_para - V1_min;
+            f = V2_sp_para - V2_min;
+            g = V3_sp_para - V3_min;
+            h = V4_sp_para - V4_min;
+            i = V1_max_perp - V1_min;
+            j = V2_max_perp - V2_min;
+            k = V3_max_perp - V3_min;
+            l = V4_max_perp - V4_min;
+            m = V1_sp_perp - V1_min;
+            n = V2_sp_perp - V2_min;
+            o = V3_sp_perp - V3_min;
+            p = V4_sp_perp - V4_min;
+
+            A = [a b c d; e f g h; i j k l; m n o p]*energy_scale
+            
+            external_potential_params = [varargin{7}(iss)-varargin{4}(iss), varargin{6}(iss)-varargin{4}(iss),...
+                                         varargin{7}(iss)-varargin{4}(iss), varargin{5}(iss)-varargin{4}(iss)]'
+            
+            Vf_fit = A\external_potential_params;
+            
+            Vfourier = (Vf_fit(1)*Vf1_array + Vf_fit(2)*Vf2_array + ...
+                        Vf_fit(3)*Vf3_array + Vf_fit(4)*Vf4_array);
+            Vfourier_scaled = Vfourier*energy_scale
+        else
+            Vfourier = Fourier_Terms(gterms, zmin_nondim, glat);
+            Vfourier_scaled = Vfourier*energy_scale*prefactor;
+        end
+
+        % sets how fine the grid is for each unit cell
+        mx = 24;
+        my = 24;
+        % mulitplied by 1.001 to make sure the range reaches the end
+        % x = [-sqrt(3)*a0_nondim : sqrt(3)*a0_nondim/(mx) : 1.001*sqrt(3)*a0_nondim - sqrt(3)*a0_nondim/(mx)];
+        % y = [-1.5*a0_nondim : 1.5*a0_nondim/(my) : 1.001*1.5*a0_nondim - 1.5*a0_nondim/(my)];
+
+        % calculates the He-Graphene potential in x and y
+        % Vr = real(V_fourier(x, y, gterms, Vfourier_scaled, 1, glat));
+
+        % V_dimensionless = Vr';
+
+        % change of variables to basis with axis aligned along the lattice
+        % vectors
+        xlat_x = glat.a1(1); 
+        xlat_y = glat.a1(2); 
+        ylat_x = glat.a2(1);
+        ylat_y = glat.a2(2);
+
+        % sets how fine the grid is along each axis
+        Mx_singlePeriod = 12;
+        My_singlePeriod = 12;
+
+        % calculates the step along each axis
+        dxlat_x = xlat_x / Mx_singlePeriod; 
+        dxlat_y = xlat_y / Mx_singlePeriod; 
+        dylat_x = ylat_x / My_singlePeriod;
+        dylat_y = ylat_y / My_singlePeriod;
+
+        % number of unit cells along each axis
+        % equal to 2x Qx Qy in make_bloch_bands
+        MperiodsXlat = 24;
+        MperiodsYlat = 24;
+
+        % makes grid in new basis
+        Lxlat_x = MperiodsXlat * xlat_x; 
+        Lxlat_y = MperiodsXlat * xlat_y; 
+        Lylat_x = MperiodsYlat * ylat_x;
+        Lylat_y = MperiodsYlat * ylat_y;
+
+        xlat_x = [-Lxlat_x/2 : dxlat_x : Lxlat_x/2 + dxlat_x/4];    
+        xlat_y = [-Lxlat_y/2 : dxlat_y : Lxlat_y/2 + dxlat_y/4]; 
+        ylat_x = [-Lylat_x/2 : dylat_x : Lylat_x/2 + dylat_x/4]; 
+        ylat_y = [-Lylat_y/2 : dylat_y : Lylat_y/2 + dylat_y/4]; 
+
+        Mx_ones = ones(length(xlat_x));
+        My_ones = ones(length(ylat_x));
+
+        xgrid = zeros(length(xlat_x),length(xlat_x));
+        ygrid = zeros(length(ylat_x),length(ylat_x));
+
+        for ix = 1:size(xgrid, 1)
+            for iy = 1:size(ygrid, 2)
+                xgrid(ix, iy) = xlat_x(ix) + ylat_x(iy);
+                ygrid(ix, iy) = xlat_y(ix) + ylat_y(iy);
+            end
+        end
+
+        % calculates the potential in the xlat-ylat grid
+        Vr_lat = real(V_fourier_grid(xgrid, ygrid, gterms, Vfourier_scaled, 1, glat));
+
+        V_dimensionless_lat = Vr_lat';
+
+        a0 = a0_real;
+        x = 2*[-sqrt(3)*a0/2 : sqrt(3)*a0/(2*mx) : sqrt(3)*a0/2 - sqrt(3)*a0/(4*mx)];
+        y = 2*[-1.5*a0/2 : 1.5*a0/(2*my) : 1.5*a0/2 - 1.5*a0/(4*my)];
+
+        Vr = real(V_fourier(x, y, gterms, Vfourier, energy_scale*prefactor, glat));
+
+        V_dimensionless = Vr';
+
+        % for the averaging in z case
+        if nargin == 5
+            % sets the arrays where the potential and fc's are accumulated 
+            V = 0*V_dimensionless;
+            V_lat = 0*V_dimensionless_lat;
+            V_fc = 0*real(Vfourier_scaled);
+
+            % gets the z and rho
+            zs = varargin{4}(iss,:)*space_normalization; % make sure to convert to dimensionless coords
+            rhos = varargin{5}(iss,:);
+
+            % loops over z
+            for iz = 1:length(zs)
+                
+                % sets the number of fourier components taken into account for the
+                % calculations and calculates the fourier components
+                gterms = 2;
+                Vfourier = Fourier_Terms(gterms, zs(iz), glat);
+
+                % scale the fourier components
+                energy_scale = k_B/E_R;
+                Vfourier_scaled = Vfourier*energy_scale*prefactor;
+                V_fc = V_fc + real(Vfourier_scaled)*rhos(iz); % adds to weighted average
+
+                % calculates the potential in the xlat-ylat grid
+                Vr_lat = real(V_fourier_grid(xgrid, ygrid, gterms, Vfourier_scaled, 1, glat));
+
+                V_dimensionless_lat = Vr_lat';
+                V_lat = V_lat + V_dimensionless_lat*rhos(iz); % adds to weighted average
+
+                % calculate the potential on a square grid
+                Vr = real(V_fourier(x, y, gterms, Vfourier, energy_scale*prefactor, glat));
+
+                V_dimensionless = Vr';
+                V =  V + V_dimensionless*rhos(iz); % adds to weighted average
+            end
+        else
+            % the outputs when not averaging in z
+            V = V_dimensionless;
+            V_lat = V_dimensionless_lat;
+            V_fc = real(Vfourier_scaled);
+        end
+        
+        % outputs
+        lattice_vectors = [glat.a1; glat.a2; glat.g1; glat.g2];
+        grid = [x; y];
+        param = [space_normalization, energy_scale];
+
+        % writes output files
+        dlmwrite(strcat("V_graphene", strain_string, ".txt"), V, 'delimiter', ',', 'precision', 16)
+
+        dlmwrite(strcat("V_graphene_lat", strain_string, ".txt"), V_lat, 'delimiter', ',', 'precision', 16)
+
+        dlmwrite(strcat("V_fourier", strain_string, ".txt"), real(V_fc), 'delimiter', ',', 'precision', 16)
+
+        dlmwrite(strcat("Lattice_Vectors", strain_string, ".txt"), lattice_vectors, 'delimiter', ',', 'precision', 16)
+
+        dlmwrite(strcat("Julia_grid", strain_string, ".txt"), grid, 'delimiter', ',', 'precision', 16)
+
+        dlmwrite(strcat("Julia_params", strain_string, ".txt"), param, 'delimiter', ',', 'precision', 16)
+        
+        iss = iss + 1;
+    end
+end
